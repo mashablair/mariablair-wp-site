baf13fc6bd7767f8f94f4b93ccbca27b69a705c5
Maria Blair <maria.klachko@gmail.com>
2016-10-05 15:41:30 -0700

    Edited content.php file by adding date, author and category info for every blog post.

--- wp-content/themes/mariablairtheme/skillcrushstarter/content.php
+++ wp-content/themes/mariablairtheme/skillcrushstarter/content.php
@@ -1,6 +1,7 @@
 <article id="post-<?php the_ID(); ?>" class="post-entry">
 	<div class="entry-wrap">
 		<header class="entry-header">
+			<h3 class="entry-time"><?php the_date(); ?></h3>
 			<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 		</header>
 		<div class="entry-summary">
@@ -8,7 +9,13 @@
 		</div>
 		<footer class="entry-footer">
 			<div class="entry-meta">
-				<span class="entry-terms comments"><?php echo get_comments_number(); ?> comments</span>
+				<span class="entry-terms comments author">
+					Written by <?php the_author(); ?>
+					/
+					Posted in <?php the_category(', '); ?>
+					/
+					<?php echo get_comments_number(); ?> comments
+				</span>
 			</div>
 		</footer>
 	</div>
